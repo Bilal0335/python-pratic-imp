82afcf1e43051eab653a20c79dd3170db4b82d39
bilal <bilalhussain42201@gmail.com>
2023-11-01 00:20:17 +0500

    split and nested game python

---
+++ split_python.py
@@ -0,0 +1,10 @@
+# import random
+
+# name = input("Enter a everybody's name separated by comma(,):\n").title()
+# name_list = name.split(',')
+# length = len(name_list)
+# random_choice = random.randint(0,length-1)
+# print(f"{name_list[random_choice]} will pay the bill.")
+
+
+
